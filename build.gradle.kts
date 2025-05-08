cca60b03e5ec75c17be01fdfaaf646f11dcc1a3f
louisco124 <157471168+louisco124@users.noreply.github.com>
2025-05-07 20:14:04 -0500

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    alias(libs.plugins.android.application) version "8.2.2" apply false
+    kotlin("android") version "1.9.0" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
